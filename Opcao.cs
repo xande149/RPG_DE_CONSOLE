1ce8363561877d9312978cdabd004704930b7f38
Krause <artes.ak@outlook.com>
2023-03-03 21:53:40 -0300

    ATUALIZAÇÃO 1.2

---
+++ Opcao.cs
@@ -0,0 +1,15 @@
+namespace RPG_DE_CONSOLE
+{
+
+internal class opcao
+{
+
+    public int Id { get; set; }
+
+    public string? Pergunta { get; set; }
+
+    public List<string> Respostas { get; set; }
+
+}
+
+}
---
+++ Resposta.cs
@@ -0,0 +1,13 @@
+namespace RPG_DE_CONSOLE
+{
+    internal class Resposta
+    {
+        public string Texto { get; set; }   
+
+        public int Destino { get; set; }
+
+    }
+
+
+
+}
